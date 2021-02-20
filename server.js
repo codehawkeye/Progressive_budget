a3c37d7461440ca68aafcec4ba90b3662bcb120e
Mike Carter <elite.mikecarter@gmail.com>
2021-02-19 19:32:09 -0500

    changed local host back to project name

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/arcane-woodland-83005', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
